e589b2d28a503fff47e027e048ebedde4b0eb9d0
Jackson Foo <jackson.foo@myboost.com.my>
2020-05-15 16:00:10 +0800

    1. Initial Commit

---
+++ IOSBoostSDK.framework/Headers/IOSBoostSDK.h
@@ -0,0 +1,19 @@
+//
+//  IOSBoostSDK.h
+//  IOSBoostSDK
+//
+//  Created by A2341 on 04/10/2019.
+//  Copyright © 2019 Boost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IOSBoostSDK.
+FOUNDATION_EXPORT double IOSBoostSDKVersionNumber;
+
+//! Project version string for IOSBoostSDK.
+FOUNDATION_EXPORT const unsigned char IOSBoostSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOSBoostSDK/PublicHeader.h>
+
+
---
+++ Pods/Cache/Source/Shared/Extensions/Date+Extensions.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/**
+ Helper NSDate extension.
+ */
+extension Date {
+
+  /// Checks if the date is in the past.
+  var inThePast: Bool {
+    return timeIntervalSinceNow < 0
+  }
+
+}
